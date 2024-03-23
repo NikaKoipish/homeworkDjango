052d0b75af27ea6585c0b42a65e6c537c92e4c69
koipish <koipish@gmail.com>
2024-03-23 12:28:48 +0300

    write data to file

--- catalog/views.py
+++ catalog/views.py
@@ -6,7 +6,9 @@ def index_contacts(request):
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
-        print(f'{name} ({phone}): {message}')
+        with open('data.txt', 'a', encoding='UTF-8') as f:
+            f.write(f'{name} ({phone}): {message}'+'\n')
+
     return render(request, 'catalog/index_contacts.html')
 
 def index_home(request):
