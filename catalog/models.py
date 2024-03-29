5f9256035ac4e4006840edb4646adbc78a1780f5
koipish <koipish@gmail.com>
2024-03-29 18:38:33 +0300

    added models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='имя')
+    description = models.TextField( verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} - {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField( verbose_name='Описание')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, verbose_name='id')
+    price = models.IntegerField(verbose_name='Цена за покупку')
+    date_of_creation = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_of_edition = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.category} {self.name} - {self.price}'
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
+        ordering = ('price',)
+
